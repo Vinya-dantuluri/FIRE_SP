35e6e96dee914b9576a24120fa42cfb4c7e2324f
adityajayanti <30630319+adityajayanti@users.noreply.github.com>
2020-11-25 11:50:35 +0530

    Demo

---
+++ Demo/Secur Data/Generator/AccessDB_Generator.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\ProgramData\Anaconda3\python.exe "C:\Users\kameshpavanj\Desktop\Secur Data\Logic\Script\Accessdb.py"
+pause
---
+++ Demo/Secur Data/Generator/KMP_Generator.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\ProgramData\Anaconda3\python.exe "C:\Users\kameshpavanj\Desktop\Secur Data\Logic\Script\KMP.py"
+pause
